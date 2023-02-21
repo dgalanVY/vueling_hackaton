57b24c731fb400533bf61913c314b7ac9adce149
Daniel Galan <daniel.galan@col.vueling.com>
2023-02-21 13:42:27 +0100

    [Feat] Changed setContentView of MainActivity

--- app/src/main/java/com/dgalan/vueling_hackaton/common/mainView/MainActivity.kt
+++ app/src/main/java/com/dgalan/vueling_hackaton/common/mainView/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.dgalan.vueling_hackaton
+package com.dgalan.vueling_hackaton.common.mainView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dgalan.vueling_hackaton.R.layout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
     }
 }
