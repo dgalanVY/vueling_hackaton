c09ba39ebe7799d647509c316161cc0c97069956
Daniel Galan <daniel.galan@col.vueling.com>
2023-02-22 10:24:02 +0100

    [Feat] new token

--- app/src/main/java/com/dgalan/vueling_hackaton/logistic/data/api/LogisticService.kt
+++ app/src/main/java/com/dgalan/vueling_hackaton/logistic/data/api/LogisticService.kt
@@ -8,10 +8,10 @@ import retrofit2.http.Query
 
 interface LogisticService {
 
-    @Headers("authorization:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQXV0aGVudGljYXRpb25BZG1pbiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6InlvdXJAZW1haWwuY29tIiwianRpIjoiMDAzNDg4N2QtYWFhZS00NDE3LTgwMzMtYmE5ZGQ1YzdjZTc5IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiQWRtaW4iLCJleHAiOjE2NzcwMjc4MTcsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMSIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJ9.YeA4p-29RZBzkWNoBKULvCPB_VJL14Go07ipGNM3iOg")
+    @Headers("authorization:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVnVlbGluZ0VtcGxveWVlVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6InRlc3RAdnVlbGluZ2VtcGxveWVlLmNvbSIsImp0aSI6IjY2NDZmNTA0LWY3M2ItNDM2Zi04Nzk3LTMxNDM0NDM0ZTkwZSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlVzZXIiLCJleHAiOjE2NzcwNjU5MzksImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMSIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJ9.bIVL6MqZYbLmVSCej78tYdT7M3-43ZHwlx83HezPIwI")
     @GET("/Handling")
     suspend fun getLogisticList(
-        @Query("startDate") startDate: String = "2023/02/02",
+        @Query("startDate") startDate: String = "2023/02/01",
         @Query("endDate") endDate: String = "2023/02/21"
     ): Response<LogisticResultDto?>
 }
